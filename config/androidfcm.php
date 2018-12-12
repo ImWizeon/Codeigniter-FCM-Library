bef5e846a9ed6e4fb00f6abe72c1be90dc5e238b
Abhishek <abhishek@meridian.net.in>
2018-12-12 13:02:14 +0530

    Config file

---
+++ config/androidfcm.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+/*
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+|| Android Firebase Push Notification Configurations
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Firebase API Key
+|--------------------------------------------------------------------------
+|
+| The secret key for Firebase API
+|
+ */
+$config['key'] = 'AAAAaFWWUSI:APA91bEgnBio-mJsDdpPfYvBJFGWNddMGgZE5DS9hWCu7ENNaBx-HKWpTGxLPVVrk2XDU318KOwleJyqBlA7laTP3QCSEEH3JsbTgaqf_XP8QybuUBAEJhHgF7qnypwyiPuAswohtEgi';
+
+/*
+|--------------------------------------------------------------------------
+| Firebase Cloud Messaging API URL
+|--------------------------------------------------------------------------
+|
+| The URL for Firebase Cloud Messafing
+|
+ */
+
+$config['fcm_url'] = 'https://fcm.googleapis.com/fcm/send';
