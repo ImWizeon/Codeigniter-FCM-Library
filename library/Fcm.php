be67fed295f9b3a912813904775b98ff3b6317d0
Abhishek <abhishek@meridian.net.in>
2018-12-12 13:02:04 +0530

    Library file

---
+++ library/Fcm.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Fcm
+{
+
+    // push message title
+    private $title;
+    private $message;
+    private $image;
+    // push message payload
+    private $data;
+    // flag indicating whether to show the push
+    // notification or not
+    // this flag will be useful when perform some opertation
+    // in background when push is recevied
+    private $is_background;
+
+    public function __construct()
+    {
+
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function setImage($imageUrl)
+    {
+        $this->image = $imageUrl;
+    }
+
+    public function setPayload($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setIsBackground($is_background)
+    {
+        $this->is_background = $is_background;
+    }
+
+    /*
+    Generating the push message array
+    */
+    public function getPush()
+    {
+        $res = array();
+        $res['data']['title'] = $this->title;
+        $res['data']['is_background'] = $this->is_background;
+        $res['data']['message'] = $this->message;
+        $res['data']['image'] = $this->image;
+        $res['data']['payload'] = $this->data;
+        $res['data']['timestamp'] = date('Y-m-d G:i:s');
+        return $res;
+    }
+
+    // sending push message to single user by firebase reg id
+    public function send($to, $message)
+    {
+        $fields = array(
+            'to' => $to,
+            'data' => $message,
+        );
+        return $this->sendPushNotification($fields);
+    }
+
+    // Sending message to a topic by topic name
+    public function sendToTopic($to, $message)
+    {
+        $fields = array(
+            'to' => '/topics/' . $to,
+            'data' => $message,
+        );
+        return $this->sendPushNotification($fields);
+    }
+
+    // sending push message to multiple users by firebase registration ids
+    public function sendMultiple($registration_ids, $message)
+    {
+        $fields = array(
+            'registration_ids' => $registration_ids,
+            'data' => $message,
+        );
+
+        return $this->sendPushNotification($fields);
+    }
+
+    // function makes curl request to firebase servers
+    private function sendPushNotification($fields)
+    {
+
+        $CI =& get_instance();
+        $CI->load->config('androidfcm'); //loading of config file
+
+        // Set POST variables
+        $url = $CI->config->item('fcm_url');
+
+        $headers = array(
+            'Authorization: key=' . $CI->config->item('key'),
+            'Content-Type: application/json',
+        );
+        // Open connection
+        $ch = curl_init();
+
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+        // Execute post
+        $result = curl_exec($ch);
+        if ($result === false) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+
+        // Close connection
+        curl_close($ch);
+
+        return $result;
+    }
+
+}
